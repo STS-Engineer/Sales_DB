0090f7df127b06dcf010325fa616432d882ef315
STS-Engineer <administration.sts@avocarbon.com>
2025-10-08 14:10:57 +0100

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine
-from .settings import settings
+from app.settings import settings
 
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, future=True)
